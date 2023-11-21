3d8f8517d4405ea0a8230fbe4f461ff04d24a99f
Gustavo Poppi <gustavopoppi@hotmail.com>
2023-11-21 17:50:54 -0300

    [DEV] Inicia desafio Dev Superior Injeção de dependência

---
+++ src/main/java/com/desafio/injecaodependencia/InjecaoDependenciaApplication.java
@@ -0,0 +1,18 @@
+package com.desafio.injecaodependencia;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjecaoDependenciaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjecaoDependenciaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
